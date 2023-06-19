6b41c2807d3565c02f0fcfc8d3efdd3c16946585
_el.evenDT <cminh.truong@gmail.com>
2023-06-19 21:20:49 +0200

    first commit

---
+++ app/src/main/java/app/eleven/cryptonow/MainActivity.kt
@@ -0,0 +1,46 @@
+package app.eleven.cryptonow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.eleven.cryptonow.ui.theme.CryptoNowTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			CryptoNowTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
+					Greeting("Android")
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+	CryptoNowTheme {
+		Greeting("Android")
+	}
+}

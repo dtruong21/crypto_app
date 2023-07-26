b5cee0fe12b39193b31facd25bd493ea8405c237
_el.evenDT <cminh.truong@gmail.com>
2023-07-26 08:57:01 +0200

    Update Unit test part 2

--- app/src/test/java/app/eleven/cryptonow/CryptoViewModelTest.kt
+++ app/src/test/java/app/eleven/cryptonow/CryptoViewModelTest.kt
@@ -7,15 +7,13 @@ import app.eleven.cryptonow.domain.interactor.get_cryptos.GetCryptosUC
 import app.eleven.cryptonow.domain.interactor.get_markets_by_crypto.GetMarketsByCryptoUC
 import app.eleven.cryptonow.domain.model.Crypto
 import app.eleven.cryptonow.presentation.get_crypto_list.CryptoViewModel
-import app.eleven.cryptonow.presentation.get_crypto_list.UiState
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Rule
-import org.junit.Assert.*
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
 
 class CryptoViewModelTest {
 
@@ -34,8 +32,10 @@ class CryptoViewModelTest {
 	fun `get Crypto List with success`() {
 		viewModel.getCryptos()
 		whenever(interactor.getCryptosUC.invoke()).thenReturn(flow { emit(Resource.Success(emptyList())) })
-		val uiState = viewModel.uiState.value
-		assertEquals(uiState.cryptos, emptyList<Crypto>())
+		runBlockingTest {
+			val uiState = viewModel.uiState.first()
+			assertEquals(uiState.cryptos, emptyList<Crypto>())
+		}
 	}
 
 	@Test
@@ -54,7 +54,7 @@ class CryptoViewModelTest {
 		viewModel.getCryptos()
 		runBlockingTest {
 			val uiState = viewModel.uiState.first()
-			assertEquals(true,uiState.isLoading)
+			assertEquals(true, uiState.isLoading)
 		}
 	}
 }
